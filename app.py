c8638b7cce7502b11972d3fda812ba2344117e29
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2022-08-29 15:42:37 +0200

    bug unknown fixed

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ print(f'{len(list_people_path)} known people in your database found.')
 
 # extract feature from know people
 list_people_encoded = []
-print('Upgrading Database...')
+print('Checking Database...')
 for face_file_name in list_people_path:
     absolute_path = os.path.join(os.getcwd(), args.known, face_file_name)
     person = cv2.imread(absolute_path)
@@ -106,102 +106,102 @@ while cap.isOpened():
             width = int((yf - yi) / 5)
             height = int((xf - xi) / 5)
 
-            if name != 'Unknown':
-                avatar = cv2.imread(f'./known_avatar/{name}.jpg')
-                avatar = cv2.resize(avatar,
-                                    (int(avatar_w_dimension * avatar.shape[1] / avatar.shape[0]), avatar_w_dimension))
+            # if name != 'Unknown2':
+            avatar = cv2.imread(f'./known_avatar/{name}.jpg')
+            avatar = cv2.resize(avatar,
+                                (int(avatar_w_dimension * avatar.shape[1] / avatar.shape[0]), avatar_w_dimension))
 
-                frame_length = frame.shape[0]
-                frame_width = frame.shape[1]
-                avatar_length = avatar.shape[0]
-                avatar_width = avatar.shape[1]
+            frame_length = frame.shape[0]
+            frame_width = frame.shape[1]
+            avatar_length = avatar.shape[0]
+            avatar_width = avatar.shape[1]
 
-                right = can_be_right(xf, frame_width, avatar_width, distance, tolerance)
-                left = can_be_left(xi, avatar_width, distance, tolerance)
-                top = can_be_top(yi, avatar_length, distance, tolerance)
-                bottom = can_be_bottom(yf, frame_length, avatar_length, distance, tolerance)
+            right = can_be_right(xf, frame_width, avatar_width, distance, tolerance)
+            left = can_be_left(xi, avatar_width, distance, tolerance)
+            top = can_be_top(yi, avatar_length, distance, tolerance)
+            bottom = can_be_bottom(yf, frame_length, avatar_length, distance, tolerance)
 
-                # draw_avatar = False
-                if top:
-                    if right:
-                        avatar_yi = yi - distance - avatar_w_dimension
-                        avatar_yf = yi - distance - avatar_w_dimension + avatar_length
-                        avatar_xi = xf + distance
-                        avatar_xf = xf + distance + avatar_width
+            # draw_avatar = False
+            if top:
+                if right:
+                    avatar_yi = yi - distance - avatar_w_dimension
+                    avatar_yf = yi - distance - avatar_w_dimension + avatar_length
+                    avatar_xi = xf + distance
+                    avatar_xf = xf + distance + avatar_width
 
-                        line_xi = xf
-                        line_xf = xf + distance
-                        line_yi = yi
-                        line_yf = yi - distance
+                    line_xi = xf
+                    line_xf = xf + distance
+                    line_yi = yi
+                    line_yf = yi - distance
 
-                        draw_avatar = True
-                    elif left:
-                        avatar_yi = yi - distance - avatar_w_dimension
-                        avatar_yf = yi - distance - avatar_w_dimension + avatar_length
-                        avatar_xi = xi - distance - avatar_w_dimension
-                        avatar_xf = xi - distance - avatar_w_dimension + avatar_width
+                    draw_avatar = True
+                elif left:
+                    avatar_yi = yi - distance - avatar_w_dimension
+                    avatar_yf = yi - distance - avatar_w_dimension + avatar_length
+                    avatar_xi = xi - distance - avatar_w_dimension
+                    avatar_xf = xi - distance - avatar_w_dimension + avatar_width
 
-                        line_xi = xi
-                        line_xf = xi - distance
-                        line_yi = yi
-                        line_yf = yi - distance
+                    line_xi = xi
+                    line_xf = xi - distance
+                    line_yi = yi
+                    line_yf = yi - distance
 
-                        draw_avatar = True
-                    else:
-                        print('to be implemented')
-                        # put in the top middle
-                elif bottom:
-                    if right:
-                        avatar_yi = yf + distance
-                        avatar_yf = yf + distance + avatar_length
-                        avatar_xi = xf + distance
-                        avatar_xf = xf + distance + avatar_width
-
-                        line_xi = xf
-                        line_xf = xf + distance
-                        line_yi = yf
-                        line_yf = yf + distance
-
-                        draw_avatar = True
-                    elif left:
-                        avatar_yi = yf + distance
-                        avatar_yf = yf + distance + avatar_length
-                        avatar_xi = xi - distance - avatar_w_dimension
-                        avatar_xf = xi - distance - avatar_w_dimension + avatar_width
-
-                        line_xi = xi
-                        line_xf = xi - distance
-                        line_yi = yf
-                        line_yf = yf + distance
-
-                        draw_avatar = True
-                    else:
-                        print('to be implemented')
-                        # put in the bottom middle
+                    draw_avatar = True
                 else:
-                    print('nothing to do at the moment!')
+                    print('to be implemented')
+                    # put in the top middle
+            elif bottom:
+                if right:
+                    avatar_yi = yf + distance
+                    avatar_yf = yf + distance + avatar_length
+                    avatar_xi = xf + distance
+                    avatar_xf = xf + distance + avatar_width
 
-                if draw_avatar:
-                    frame[avatar_yi:avatar_yf, avatar_xi:avatar_xf] = avatar
+                    line_xi = xf
+                    line_xf = xf + distance
+                    line_yi = yf
+                    line_yf = yf + distance
 
-                    cv2.line(frame, (line_xi, line_yi), (line_xf, line_yf), color, thickness)
+                    draw_avatar = True
+                elif left:
+                    avatar_yi = yf + distance
+                    avatar_yf = yf + distance + avatar_length
+                    avatar_xi = xi - distance - avatar_w_dimension
+                    avatar_xf = xi - distance - avatar_w_dimension + avatar_width
 
-                    cv2.rectangle(frame, (avatar_xi, avatar_yi), (avatar_xf, avatar_yf), color,
-                                  thickness)
+                    line_xi = xi
+                    line_xf = xi - distance
+                    line_yi = yf
+                    line_yf = yf + distance
 
-                cv2.putText(frame, name, (xi, yi - 20), cv2.FONT_HERSHEY_DUPLEX, 0.65, (0, 200, 0), 1)
+                    draw_avatar = True
+                else:
+                    print('to be implemented')
+                    # put in the bottom middle
+            else:
+                print('nothing to do at the moment!')
 
-                cv2.line(frame, (xi, yi), (xi + width, yi), color, thickness)
-                cv2.line(frame, (xi, yi), (xi, yi + height), color, thickness)
+            if draw_avatar:
+                frame[avatar_yi:avatar_yf, avatar_xi:avatar_xf] = avatar
 
-                cv2.line(frame, (xf, yf), (xf - width, yf), color, thickness)
-                cv2.line(frame, (xf, yf), (xf, yf - height), color, thickness)
+                cv2.line(frame, (line_xi, line_yi), (line_xf, line_yf), color, thickness)
 
-                cv2.line(frame, (xi, yf), (xi + width, yf), color, thickness)
-                cv2.line(frame, (xi, yf), (xi, yf - height), color, thickness)
+                cv2.rectangle(frame, (avatar_xi, avatar_yi), (avatar_xf, avatar_yf), color,
+                              thickness)
 
-                cv2.line(frame, (xf, yi), (xf - width, yi), color, thickness)
-                cv2.line(frame, (xf, yi), (xf, yi + height), color, thickness)
+            cv2.putText(frame, name, (avatar_xf+10, avatar_yi + 20), cv2.FONT_HERSHEY_DUPLEX, 0.65, (0, 230, 0), 1)
+
+            cv2.line(frame, (xi, yi), (xi + width, yi), color, thickness)
+            cv2.line(frame, (xi, yi), (xi, yi + height), color, thickness)
+
+            cv2.line(frame, (xf, yf), (xf - width, yf), color, thickness)
+            cv2.line(frame, (xf, yf), (xf, yf - height), color, thickness)
+
+            cv2.line(frame, (xi, yf), (xi + width, yf), color, thickness)
+            cv2.line(frame, (xi, yf), (xi, yf - height), color, thickness)
+
+            cv2.line(frame, (xf, yi), (xf - width, yi), color, thickness)
+            cv2.line(frame, (xf, yi), (xf, yi + height), color, thickness)
 
     cv2.imshow('Face Recognition', frame)
 
